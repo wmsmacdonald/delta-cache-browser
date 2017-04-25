bba0a88a0d142c431af1eaa203e7c5ee2b4d942a
William MacDonald <wmsmacdonald@gmail.com>
2017-04-25 01:12:28 -0500

    added comments

--- dist/delta_cache_sw.js
+++ dist/delta_cache_sw.js
@@ -55,25 +55,23 @@
 	  self.skipWaiting();
 	});
 
-	let deltaCache; // global so fetch event can access
-
 	self.addEventListener('activate', function(event) {
+	  // extend lifetime of event until cache is deleted
 	  event.waitUntil(
-	    // deletes cache to prevent incompatiblity between requests (not necessary in production)
-	    // opens new cache before listening for fetch events
-	    caches.delete(CACHE_NAME).then(() => caches.open(CACHE_NAME)).then(cache => {
-	      deltaCache = cache;
-	    })
+	    // deletes cache to prevent incompatiblity between requests
+	    caches.delete(CACHE_NAME)
 	  );
 	});
 
-	self.onfetch = function(event) {
+	self.addEventListener('fetch', function(event) {
 	  event.respondWith(
-	    // function is a proxy between the client and the server
-	    // returns a promise that contains a response
-	    fetchProxy(deltaCache, event.request)
+	    caches.open(CACHE_NAME).then(
+	      // a proxy between the client and the server
+	      // returns a promise that contains a response
+	      fetchProxy.bind(null, event.request, self.registration.scope)
+	    )
 	  );
-	}
+	});
 
 
 
@@ -87,42 +85,48 @@
 	const deltaFetch = __webpack_require__(3);
 	const fetchUtil = __webpack_require__(4);
 
-	function fetchProxy(cache, originalRequest) {
+	function fetchProxy(originalRequest, scope, cache) {
 	  // get cached response which matches the request
 	  const serverResponseP = cache.match(originalRequest).then(cachedResponse => {
 
 	    // request cached and has same origin
-	    if (urlUtil.isSameOrigin(originalRequest.url, self.registration.scope) && cachedResponse !== undefined) {
+	    if (urlUtil.isSameOrigin(originalRequest.url, scope)) {
 	      return fetch(
 	        // create delta request with cached ETag
 	        deltaFetch.createDeltaRequest(originalRequest, cachedResponse.headers.get('ETag'))
-	      ).then(serverResponse => {
-
-	        // server sent a patch (rather than the full file)
-	        if (serverResponse.status === 226 && serverResponse.headers.get('Delta-Base') === cachedResponse.headers.get('ETag')) {
-	          // use the patch on the cached file to create an updated response
-	          return deltaFetch.patchResponse(serverResponse, cachedResponse);
-	        }
-	        // no change from cached version
-	        else if (serverResponse.status === 304) {
-	          return Promise.resolve(deltaFetch.convert304to200(cachedResponse, serverResonse.headers));
-	        }
-	        else {
-	          return Promise.reject('Server gave unrecongized status code: ' + serverResonse.status);
-	        }
-	      });
+	      ).then(processServerResponse.bind(null, cachedResponse));
 	    }
 	    else {
-	      // normal request without delta caching
+	      // different origin, do normal request
 	      return fetch(originalRequest);
 	    }
 	  })
+	    // not in cache, no delta encoding available, do normal request
+	    .catch(() => fetch(originalRequest));
 	  
 	  serverResponseP.then(response => {
 	    deltaFetch.cacheIfHasEtag(cache, originalRequest, response.clone());
 	  })
 
 	  return serverResponseP;
+	}
+
+	// handle delta encoded and normal responses, return promise containing Response object
+	function processServerResponse(cachedResponse, serverResponse) {
+
+	  // server sent a patch (rather than the full file)
+	  if (serverResponse.status === 226 && serverResponse.headers.get('Delta-Base') === cachedResponse.headers.get('ETag')) {
+	    // use the patch on the cached file to create an updated response
+	    return deltaFetch.patchResponse(serverResponse, cachedResponse);
+	  }
+	  // no change from cached version
+	  else if (serverResponse.status === 304) {
+	    return Promise.resolve(deltaFetch.convert304to200(cachedResponse, serverResonse.headers));
+	  }
+	  // no delta encoding
+	  else {
+	    return Promise.resolve(serverResponse);
+	  }
 	}
 
 	module.exports = fetchProxy;
