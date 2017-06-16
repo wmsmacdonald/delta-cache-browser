e7dcc9e0a8d9112e1790117831aa960fc260adf6
William MacDonald <wmsmacdonald@gmail.com>
2017-06-16 13:08:18 -0700

    switched routes in server.js

--- demo/server.js
+++ demo/server.js
@@ -11,21 +11,20 @@ const deltaCache = createDeltaCache();
 const app = express();
 app.use(express.static(__dirname + '/public'));
 
-
 const text = fs.readFileSync(__dirname + '/sample_text/loremipsum.html')
-
-// no delta encoding
-app.get('/normalContent', (req, res) => {
-  const time = new Buffer(new Date().toTimeString())
-  const dynamicText = Buffer.concat([time, text])
-  res.end(dynamicText)
-});
 
 // use delta encoding (when possible)
 app.get('/deltaEncodedContent', (req, res) => {
   const time = new Buffer(new Date().toTimeString())
   const dynamicText = Buffer.concat([time, text])
   deltaCache.respondWithDeltaEncoding(req, res, dynamicText);
+});
+
+// no delta encoding
+app.get('/normalContent', (req, res) => {
+  const time = new Buffer(new Date().toTimeString())
+  const dynamicText = Buffer.concat([time, text])
+  res.end(dynamicText)
 });
 
 const server = app.listen(4555, (err) => {
